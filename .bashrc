1e3509d953013548c555e39a9576417fa71463ec
dhanu perera <dhanut@tuta.io>
2024-09-23 17:54:49 +0530

    chore: adding dotfiles to the git

---
+++ .bashrc
@@ -0,0 +1,16 @@
+# Aliases
+alias ls='lsd -F'
+alias la="lsd -a1"
+alias rm='rm -I'
+alias super-productivity='env -u WAYLAND_DISPLAY super-productivity'
+alias brave='env -u WAYLAND_DISPLAY brave'
+alias logseq='env -u WAYLAND_DISPLAY logseq'
+
+source ~/.exports.sh
+
+# Initialising starship 
+eval "$(starship init bash)"
+if [ -f "/home/dhanut/.config/fabric/fabric-bootstrap.inc" ]; then . "/home/dhanut/.config/fabric/fabric-bootstrap.inc"; fi
+
+# Adding fzf keybindings and fuzzy completion
+eval "$(fzf --bash)"
---
+++ .exports.sh
@@ -0,0 +1,62 @@
+# Exporting environtment variables
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_STATE_HOME="$HOME/.local/state"
+export XDG_CACHE_HOME="$HOME/.local/share"
+
+# Other env variables
+export HISTFILE="${XDG_STATE_HOME}"/bash/history
+export CALCHISTFILE="$XDG_CACHE_HOME"/calc_history
+export DOCKER_CONFIG="$XDG_CONFIG_HOME"/docker  
+export GNUPGHOME="$XDG_DATA_HOME"/gnupg
+export GTK2_RC_FILES="$XDG_CONFIG_HOME"/gtk-2.0/gtkrcERRFILE="$XDG_CACHE_HOME/X11/xsession-errors"
+ERRFILE="$XDG_CACHE_HOME/X11/xsession-errors"
+export RUSTUP_HOME="$XDG_DATA_HOME"/rustup
+export PYTHONSTARTUP="${XDG_CONFIG_HOME}/python/pythonrc"
+export IPYTHONDIR="${XDG_CONFIG_HOME}/ipython"
+export JUPYTER_CONFIG_DIR="$XDG_CONFIG_HOME/jupyter"
+ERRFILE="$XDG_CACHE_HOME/X11/xsession-errors"
+export XCOMPOSECACHE="${XDG_CACHE_HOME}"/X11/xcompose
+
+# Startship
+export STARSHIP_CACHE=~/.starship/cache
+
+# FZF Color scheme
+export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS' --color=fg:#848d97,bg:#30363d,hl:#ffffff --color=fg+:#e6edf3,bg+:#313f50,hl+:#ffa657 --color=info:#d29922,prompt:#2f81f7,pointer:#a371f7 --color=marker:#3fb950,spinner:#6e7681,header:#495058'
+
+# Exporting paths
+export PATH="/opt/anaconda/bin:$PATH"
+export PATH="~/.local/bin:$PATH"
+export PATH="~/.cargo/bin:$PATH"
+export LIBRARY_PATH=/usr/lib:$LIBRARY_PATH
+
+# Exporting android paths
+export ANDROID_HOME="$HOME/Android/Sdk"
+#export ANDROID_SDK_ROOT="$HOME/Android/Sdk/platform-tools/34.0.0"
+export ANDROID_NDK_ROOT="$HOME/Android/Sdk/ndk/26.1.10909125"
+export PATH="$HOME/Android/tools:$PATH"
+export PATH="$HOME/Android/platform-tools:$PATH"
+export PATH="$ANDROID_HOME/cmdline-tools/latest/bin:$PATH"
+export PATH="$ANDROID_HOME/build-tools/34.0.0:$PATH"
+
+export PATH="$HOME/.scripts:$PATH"
+
+export EDITOR="nvim"
+export SUDO_EDITOR="nvim"
+
+# Browser
+export BROWSER="firefox"
+
+# Variables from Hyprland
+export XDG_SESSION_TYPE="wayland"
+export XDG_CURRENT_DESKTOP="sway"
+export XDG_SESSION_DESKTOP="sway"
+export QT_QPA_PLATFORMTHEME="qt5ct"
+export QT_QPA_PLATFORM="wayland"
+export MOZ_ENABLE_WAYLAND="1"
+export GDK_BACKEND="wayland"
+#export GDK_SCALE="0.711" # Fractional scaling breaks android studio
+export WLR_DRM_NO_ATOMIC="1"
+export _JAVA_AWT_WM_NONREPARENTING="1"
+export SDL_VIDEODRIVER="wayland"
+export CLUTTER_BACKEND="wayland"

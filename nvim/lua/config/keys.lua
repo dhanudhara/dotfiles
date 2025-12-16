5ef24258d75b81a014e8f6aebaee9cb2a5b274de
Dhanu Perera <dhanut@tuta.io>
2025-12-16 13:08:56 +0530

    feat: add keymaps

--- nvim/lua/config/keys.lua
+++ nvim/lua/config/keys.lua
@@ -1,4 +1,14 @@
---[
--- check the current file type: if filetype:match(".nix")
--- key is <leader>ft command '%!nixfmt'
---]
+local diagnostics_active = true
+
+local toggle_diagnostics = function()
+  diagnostics_active = not diagnostics_active
+  if diagnostics_active then
+    vim.diagnostic.enable(nil, { bufnr = 0 })
+  else
+    vim.diagnostic.disable(nil, { bufnr = 0 })
+  end
+end
+
+return {
+  vim.keymap.set('n', '<Leader>dt', toggle_diagnostics, { desc = "Toggle diagnostics" }),
+}

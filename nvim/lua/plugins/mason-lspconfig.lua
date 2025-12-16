1c983de611a6a4f8ef3cce3781f6d47ea2ba8983
Dhanu Perera <dhanut@tuta.io>
2025-12-16 13:09:33 +0530

    feat: add cmp-nvim

--- nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/lua/plugins/mason-lspconfig.lua
@@ -1,8 +1,39 @@
 return {
     "mason-org/mason-lspconfig.nvim",
-    opts = {},
-    dependencies = {
-        { "mason-org/mason.nvim", opts = {} },
-        "neovim/nvim-lspconfig",
+    opts = {
+        ensure_installed = {
+            "lua_ls",
+            "autopep8",
+            "kotlin-lsp",
+            "pyflakes",
+            "python-lsp-server",
+            "uv",
+            "zls",
+        },
     },
+    dependencies = {
+        { "mason-org/mason.nvim", opts = {}, },
+        "neovim/nvim-lspconfig",
+        {
+            "hrsh7th/nvim-cmp",
+            dependencies = { "hrsh7th/cmp-nvim-lsp" },
+            opts = {},
+        },
+    },
+    config = function(_, opts)
+        local mason_lspconfig = require("mason-lspconfig")
+        local lspconfig = require("lspconfig")
+        local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+        mason_lspconfig.setup({
+            ensure_installed = opts.ensure_installed,
+            handlers = {
+                function(server_name)
+                    lspconfig[server_name].setup({
+                        capabilities = capabilities,
+                    })
+                end,
+            },
+        })
+    end,
 }

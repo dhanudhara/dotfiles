e9f04f5171a32fd54b8229c14b902c4a4da374ba
Dhanu Perera <dhanut@tuta.io>
2026-02-02 12:54:37 +0530

    feat: add indent, and dap plugins

---
+++ nvim/lua/plugins/nvim-dap-python.lua
@@ -0,0 +1,7 @@
+return {
+    "mfussenegger/nvim-dap-python",
+    ft = "python",
+    config = function ()
+        require("dap-python").setup("uv")
+    end
+}
---
+++ nvim/lua/plugins/nvim-dap-virtual-text.lua
@@ -0,0 +1,8 @@
+return {
+    "theHamsta/nvim-dap-virtual-text",
+    dependencies = { "mfussenegger/nvim-dap", "nvim-treesitter/nvim-treesitter" },
+    opts = {
+        enabled = true,
+        enabled_commands = true,
+    },
+}

9c08b047ba28ac9ab74b5fe4d1cb4d062ee3fccf
Dhanu Perera <dhanut@tuta.io>
2026-02-02 12:57:50 +0530

    fix: add correct multi cursor package and keys

--- nvim/lua/plugins/multicursor.lua
+++ nvim/lua/plugins/multicursor.lua
@@ -1,3 +1,9 @@
 return {
-    "jake-stewart/multicursor.nvim",
+    "brenton-leighton/multiple-cursors.nvim",
+    version = "*",  -- Use the latest tagged version
+    opts = {},
+    keys = {
+        {"<C-j>", "<Cmd>MultipleCursorsAddDown<CR>", mode = {"n", "x"}, desc = "Add cursor and move down"},
+        {"<C-k>", "<Cmd>MultipleCursorsAddUp<CR>", mode = {"n", "x"}, desc = "Add cursor and move up"},
+    },
 }

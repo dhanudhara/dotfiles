b50f9696b56def5975c25be1f19f48fff623b92b
Dhanu Perera <dhanut@tuta.io>
2026-02-02 12:56:53 +0530

    feat: remove ensure_installed, and setup blink.cmp

--- nvim/lua/plugins/mason-lspconfig.lua
+++ nvim/lua/plugins/mason-lspconfig.lua
@@ -1,38 +1,18 @@
 return {
     "mason-org/mason-lspconfig.nvim",
     opts = {
-        ensure_installed = {
-            "lua_ls",
-            "autopep8",
-            "kotlin-lsp",
-            "pyflakes",
-            "python-lsp-server",
-            "zls",
+        -- ensure_installed = {},
+        handlers = {
+            function(server_name)
+                require("lspconfig")[server_name].setup({
+                    capabilities = require("blink.cmp").get_capabilities(),
+                })
+            end,
         },
     },
     dependencies = {
         { "mason-org/mason.nvim", opts = {}, },
         "neovim/nvim-lspconfig",
-        {
-            "hrsh7th/nvim-cmp",
-            dependencies = { "hrsh7th/cmp-nvim-lsp" },
-            opts = {},
-        },
+        "saghen/blink.cmp",
     },
-    config = function(_, opts)
-        local mason_lspconfig = require("mason-lspconfig")
-        local lspconfig = require("lspconfig")
-        local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-        mason_lspconfig.setup({
-            ensure_installed = opts.ensure_installed,
-            handlers = {
-                function(server_name)
-                    lspconfig[server_name].setup({
-                        capabilities = capabilities,
-                    })
-                end,
-            },
-        })
-    end,
 }

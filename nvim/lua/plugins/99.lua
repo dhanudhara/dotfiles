a68d78fb089aea03abc2a52cf05e92fd59a1bae9
Dhanu Perera <dhanut@tuta.io>
2026-07-01 14:11:22 +0530

    chore: update 99 plugin

--- nvim/lua/plugins/99.lua
+++ nvim/lua/plugins/99.lua
@@ -3,62 +3,55 @@ return {
     config = function()
         local _99 = require("99")
 
-        -- For logging that is to a file if you wish to trace through requests
-        -- for reporting bugs, i would not rely on this, but instead the provided
-        -- logging mechanisms within 99.  This is for more debugging purposes
         local cwd = vim.uv.cwd()
         local basename = vim.fs.basename(cwd)
-
         _99.setup({
+            -- provider = _99.Providers.ClaudeCodeProvider,  -- default: OpenCodeProvider
+            provider = _99.Providers.OpenCodeProvider,
             logger = {
                 level = _99.DEBUG,
                 path = "/tmp/" .. basename .. ".99.debug",
                 print_on_error = true,
             },
+            tmp_dir = "./tmp",
 
-            provider = _99.OpenCodeProvider,
-            model = 'opencode/minimax-m2.5-free',
-
-            --- A new feature that is centered around tags
-            {
+            --- Completions: #rules and @files in the prompt buffer
+            completion = {
                 custom_rules = {
-                  "scratch/custom_rules/",
+                  -- "scratch/custom_rules/",
                 },
-                source = "blink",
+
+                --- Configure @file completion (all fields optional, sensible defaults)
+                files = {
+                    -- enabled = true,
+                    -- max_file_size = 102400,     -- bytes, skip files larger than this
+                    -- max_files = 5000,            -- cap on total discovered files
+                    -- exclude = { ".env", ".env.*", "node_modules", ".git", ... },
+                },
+
+                --- What autocomplete engine to use. Defaults to native (built-in) if not specified.
+                source = "native", -- "native" (default), "cmp", or "blink"
             },
 
-            --- WARNING: if you change cwd then this is likely broken
-            --- ill likely fix this in a later change
-            ---
-            --- md_files is a list of files to look for and auto add based on the location
-            --- of the originating request.  That means if you are at /foo/bar/baz.lua
-            --- the system will automagically look for:
-            --- /foo/bar/AGENT.md
-            --- /foo/AGENT.md
-            --- assuming that /foo is project root (based on cwd)
             md_files = {
-                "AGENT.md",
+                -- "AGENT.md",
             },
         })
 
-        -- take extra note that i have visual selection only in v mode
-        -- technically whatever your last visual selection is, will be used
-        -- so i have this set to visual mode so i dont screw up and use an
-        -- old visual selection
-        --
-        -- likely ill add a mode check and assert on required visual mode
-        -- so just prepare for it now
         vim.keymap.set("v", "<leader>9v", function()
             _99.visual()
-        end)
+        end, { desc = "Send visual selection to 99" })
 
-        --- if you have a request you dont want to make any changes, just cancel it
-        vim.keymap.set("v", "<leader>9x", function()
+        vim.keymap.set("n", "<leader>9x", function()
             _99.stop_all_requests()
-        end)
+        end, { desc = "Stop all 99 requests" })
 
         vim.keymap.set("n", "<leader>9s", function()
-				_99.search()
-			end)
+            _99.search()
+        end, { desc = "Search 99 conversations" })
+
+        vim.keymap.set("n", "<leader>9p", function()
+            require("99.extensions.telescope").select_model()
+        end, { desc = "Select 99 model via Telescope" })
     end,
 }
